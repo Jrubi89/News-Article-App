4eb84f8e0bfff79dbf5c6458f5c5f6f451cf952e
Jessica Rubio <jrubi89@icloud.com>
2020-08-06 22:46:45 -0400

    creating News modal

--- models/Comment.js
+++ models/Comment.js
@@ -1,12 +1,12 @@
  var mongoose = require('mongoose');
   var Schema = mongoose.Schema;
 
-  var NewsSchema = new Schema({
+  var CommentSchema = new Schema({
     title:  String, required: true, // String is shorthand for {type: String}
     link: String, required: true,
     comments: [{ Schema.Types.ObjectId, ref: "Comment" }],
     
   });
-var News = mongoose.model("News", NewsSchema);
-module.exports = News;
+var Comment = mongoose.model("Comment", CommentSchema);
+module.exports = Commnets;
   
