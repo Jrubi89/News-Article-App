4f74d18581065b50d441476cb9777da1362b6b5b
Jessica Rubio <jrubi89@icloud.com>
2020-08-06 22:51:36 -0400

    editing  Comment  modal

--- models/Comment.js
+++ models/Comment.js
@@ -2,10 +2,8 @@
   var Schema = mongoose.Schema;
 
   var CommentSchema = new Schema({
-    title:  String, required: true, // String is shorthand for {type: String}
-    link: String, required: true,
-    comments: [{ Schema.Types.ObjectId, ref: "Comment" }],
-    
+    name:  String, // String is shorthand for {type: String}
+    body: String, required: true,    
   });
 var Comment = mongoose.model("Comment", CommentSchema);
 module.exports = Commnets;
